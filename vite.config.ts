85c70e5ab40fc81a06fff6157b5614f2e4f42520
Goonie <github@goonie.nl>
2024-01-23 06:05:44 +0100

    Set up ~ as alias for src directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [basicSsl(), react()],
+  resolve: { alias: { '~': '/src' } },
   server: { port: 3000 },
 });
